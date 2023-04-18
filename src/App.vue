d82f2f408270edf773260a520e8ef4c196e09959
Roberto Teixeira <robertoteixeira.developer@gmail.com>
2023-04-18 09:28:27 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <TodosComponent v-bind:todos="todos"/>
+  </div>
+  {{ msg }}
+</template>
+
+<script>
+import TodosComponent from './components/TodosComponent'
+//import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TodosComponent
+  },
+  data() {
+    return {
+      todos: [
+        {
+          //Array of objects
+          id: 1,
+          title: 'Todo one',
+          completed: false
+        },
+        {
+          //Array of objects
+          id: 1,
+          title: 'Todo two',
+          completed: true
+        },
+        {
+          //Array of objects
+          id: 1,
+          title: 'Todo three',
+          completed: true
+        },
+      ]
+    }
+  }
+}
+</script>
+
+
+<style>
+
+* { 
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+</style>
+
---
+++ src/components/TodosComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+       <div v-for="todo in todos"
+       :key="todo">
+        <TodoItem v-bind:todo="todo"/>
+       </div>
+    </div>    
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue'
+
+export default {
+    name: "TodosComponent",
+    components: {
+        TodoItem
+    },
+    props: ["todos"]
+}
+</script>
+
+<style scoped>
+
+</style>
